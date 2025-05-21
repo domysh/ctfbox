b4aa511e36313011a3975f6895baddd92b682d01
Domingo Dirutigliano <me@domy.sh>
2025-05-21 11:10:09 +0200

    fixed some imgs for firefox

--- gameserver/frontend/src/components/MainLayout.tsx
+++ gameserver/frontend/src/components/MainLayout.tsx
@@ -1,89 +1,118 @@
-import { AppShell, Box, Button, Container, Image, Loader, LoadingOverlay, Space, Title } from "@mantine/core"
-import { Link } from "react-router-dom"
-import { RulesContent } from "../pages/RulesContent"
-import { ScoreboardPage } from "../pages/ScoreboardPage"
-import { ScoreboardTeamDetail } from "../pages/ScoreboardTeamDetail"
-import { useStatusQuery } from "../scripts/query"
-import { useEffect, useMemo, useState } from "react"
-import { useQueryClient } from "@tanstack/react-query"
-import { NotFoundContent } from "./NotFoundContent"
-import { useGlobalState } from "../scripts/utils"
+import {
+    AppShell,
+    Box,
+    Button,
+    Container,
+    Loader,
+    LoadingOverlay,
+    Space,
+    Title,
+} from "@mantine/core";
+import { Link } from "react-router-dom";
+import { RulesContent } from "../pages/RulesContent";
+import { ScoreboardPage } from "../pages/ScoreboardPage";
+import { ScoreboardTeamDetail } from "../pages/ScoreboardTeamDetail";
+import { useStatusQuery } from "../scripts/query";
+import { useEffect, useMemo, useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { NotFoundContent } from "./NotFoundContent";
+import { useGlobalState } from "../scripts/utils";
 
-type Pages = "rules" | "scoreboard" | "scoreboard-team" | "not-found" | "loading"
+type Pages =
+    | "rules"
+    | "scoreboard"
+    | "scoreboard-team"
+    | "not-found"
+    | "loading";
 
-export const MainLayout = ({ page }: { page:Pages }) => {
-    const config = useStatusQuery()
-    const [oldRound, setOldRound] = useState(-1)
-    const queryClient = useQueryClient()
+export const MainLayout = ({ page }: { page: Pages }) => {
+    const config = useStatusQuery();
+    const [oldRound, setOldRound] = useState(-1);
+    const queryClient = useQueryClient();
 
-    const { headerComponents, setHeaderComponents, loading } = useGlobalState()
+    const { headerComponents, setHeaderComponents, loading } = useGlobalState();
 
-    useEffect(()=>{
+    useEffect(() => {
         if (!config.isFetching && oldRound != config.data?.current_round) {
-            setOldRound(config.data?.current_round??-1)
+            setOldRound(config.data?.current_round ?? -1);
             queryClient.invalidateQueries({
-                queryKey: ["scoreboard"]
-            })
+                queryKey: ["scoreboard"],
+            });
         }
-    }, [config.isFetching])
+    }, [config.isFetching]);
 
-    useEffect(()=>{
-      if (!["scoreboard-team"].includes(page)) {
-        setHeaderComponents(null)
-      }
-    }, [page])
+    useEffect(() => {
+        if (!["scoreboard-team"].includes(page)) {
+            setHeaderComponents(null);
+        }
+    }, [page]);
 
     const renderedPage = useMemo(() => {
-        if (page == "not-found") return <NotFoundContent key="not-found" />
-        if (page == "rules") return <RulesContent key="rules" />
-        if (page == "scoreboard") return <ScoreboardPage key="scoreboard" />
-        if (page == "scoreboard-team") return <ScoreboardTeamDetail key="scoreboard-team" />
-        return <Loader size={40} />
-    }, [page])
+        if (page == "not-found") return <NotFoundContent key="not-found" />;
+        if (page == "rules") return <RulesContent key="rules" />;
+        if (page == "scoreboard") return <ScoreboardPage key="scoreboard" />;
+        if (page == "scoreboard-team")
+            return <ScoreboardTeamDetail key="scoreboard-team" />;
+        return <Loader size={40} />;
+    }, [page]);
 
-    return <AppShell
-        header={{ height: 60 }}
-        padding="md"
-    >
-    <AppShell.Header>
-      <Box className='center-flex' style={{ height: "100%" }} >
-        <Space w="md" />
-        <Image src="/logo.png" alt="CTFBox Logo" width={40} height={40} />
-        <Title ml="5px" order={2}>
-          CTFBox
-        </Title>
-        <Box flex={1} />
-        {headerComponents}
-        <Box className="center-flex">
-          <Title order={5}>
-            <Link to="/rules">
-                <Button color="cyan" variant={page == "rules"?"filled":"outline"}>
-                    Rules
-                </Button>
-            </Link>
-          </Title>
-          <Space w="md" />
-          <Title order={5}>
-            <Link to="/scoreboard">
-                <Button color="cyan" variant={page == "scoreboard"?"filled":"outline"}>
-                    Scoreboard
-                </Button>
-            </Link>
-          </Title>
-          <Space w="md" />
-        </Box>
-      </Box>
-    </AppShell.Header>
-    <AppShell.Main style={{ position: 'relative' }}>
-        <LoadingOverlay 
-            visible={loading} 
-            zIndex={1000}
-            overlayProps={{ blur: 2 }}
-            loaderProps={{ size: 'xl', color: 'cyan' }}
-        />
-        <Container fluid>
-            {renderedPage}
-        </Container>
-    </AppShell.Main>
-  </AppShell> 
-}
+    return (
+        <AppShell header={{ height: 60 }} padding="md">
+            <AppShell.Header>
+                <Box className="center-flex" style={{ height: "100%" }}>
+                    <Space w="md" />
+                    <img
+                        src="/logo.png"
+                        alt="CTFBox Logo"
+                        width={40}
+                        height={40}
+                    />
+                    <Title ml="5px" order={2}>
+                        CTFBox
+                    </Title>
+                    <Box flex={1} />
+                    {headerComponents}
+                    <Box className="center-flex">
+                        <Title order={5}>
+                            <Link to="/rules">
+                                <Button
+                                    color="cyan"
+                                    variant={
+                                        page == "rules" ? "filled" : "outline"
+                                    }
+                                >
+                                    Rules
+                                </Button>
+                            </Link>
+                        </Title>
+                        <Space w="md" />
+                        <Title order={5}>
+                            <Link to="/scoreboard">
+                                <Button
+                                    color="cyan"
+                                    variant={
+                                        page == "scoreboard"
+                                            ? "filled"
+                                            : "outline"
+                                    }
+                                >
+                                    Scoreboard
+                                </Button>
+                            </Link>
+                        </Title>
+                        <Space w="md" />
+                    </Box>
+                </Box>
+            </AppShell.Header>
+            <AppShell.Main style={{ position: "relative" }}>
+                <LoadingOverlay
+                    visible={loading}
+                    zIndex={1000}
+                    overlayProps={{ blur: 2 }}
+                    loaderProps={{ size: "xl", color: "cyan" }}
+                />
+                <Container fluid>{renderedPage}</Container>
+            </AppShell.Main>
+        </AppShell>
+    );
+};
